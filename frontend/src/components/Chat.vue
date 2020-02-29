d225ac3866cac89313eeb361ef00e525aa5e8ccf
dorakemon <ken0326w@gmail.com>
2020-03-01 02:39:01 +0900

    完成

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -2,10 +2,13 @@
   <div class="card mt-3">
       <div class="card-body">
           <div class="card-title">
-              <h3>Chat Group</h3>
+              <h3>Todo Group</h3>
               <hr>
           </div>
           <div class="card-body">
+            <div class="test" v-for="(msg, index) in test" :key="index">
+                <p><span class="font-weight-bold">{{ msg.username }}: </span>{{ msg.content }}</p>
+            </div>
               <div class="messages" v-for="(msg, index) in messages" :key="index">
                   <p><span class="font-weight-bold">{{ msg.user }}: </span>{{ msg.message }}</p>
               </div>
@@ -23,12 +26,16 @@
               </div>
               <button type="submit" class="btn btn-success">Send</button>
           </form>
+            <!-- <p>{{messages}}</p> -->
       </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable no-console */
+
 import io from 'socket.io-client';
+import axios from "axios";
 
 export default {
     data() {
@@ -36,7 +43,9 @@ export default {
             user: '',
             message: '',
             messages: [],
-            socket : io('localhost:3001')
+            socket : io('localhost:3001'),
+
+            test: ""
         }
     },
     methods: {
@@ -54,6 +63,11 @@ export default {
         this.socket.on('MESSAGE', (data) => {
             this.messages = [...this.messages, data];
             // you can also do this.messages.push(data)
+        });
+        
+        axios.get("http://localhost:8000/api/todo/")
+            .then(response => {
+            this.test = response.data;
         });
     }
 }
