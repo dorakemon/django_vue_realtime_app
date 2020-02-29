808c557319eaf92392dcea7c9bf5accf81ab13e8
ken <ken0326w@gmail.com>
2020-03-01 00:54:58 +0900

    Django側完成

--- backend/todo/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Todo(models.Model):
-    username = models.CharField(verbose_name="名前", max_length=255)
-    content = models.CharField(verbose_name="内容", max_length=255)
-
-    def __str__(self):
-        return f'{self.username} / {self.content}'
