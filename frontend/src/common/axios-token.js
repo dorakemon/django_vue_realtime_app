11f916da57ad4c30e210f5897a384535e845f747
dorakemon <ken0326w@gmail.com>
2020-03-02 17:14:06 +0900

    vuetify導入前

--- frontend/src/common/axios-token.js
+++ frontend/src/common/axios-token.js
@@ -1,10 +1,19 @@
 import axios from 'axios'
 
+const getAuthorization = () => {
+    if (localStorage.getItem('jwt') === null){
+        return null
+    }
+    else {
+        return `JWT ${localStorage.getItem('jwt').slice(1).slice(0,-1)}`
+    }
+};
+
 const instance = axios.create({
     baseURL: 'http://localhost:8000/api/',
     headers: { 
         'Content-Type': 'application/json',
-        'Authorization': `JWT ${localStorage.getItem('jwt').slice(1).slice(0,-1)}` 
+        'Authorization': getAuthorization()
     },
     data: {}
 })
--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -1,78 +1,84 @@
 <template>
   <div class="card mt-3">
+    <div class="card-body">
+      <div class="card-title">
+        <h3>Todo Group</h3>
+        <hr />
+      </div>
       <div class="card-body">
-          <div class="card-title">
-              <h3>Todo Group</h3>
-              <hr>
-          </div>
-          <div class="card-body">
-            <div class="test" v-for="(msg, index) in test" :key="index">
-                <p><span class="font-weight-bold">{{ msg.username }}: </span>{{ msg.content }}</p>
-            </div>
-              <div class="messages" v-for="(msg, index) in messages" :key="index">
-                  <p><span class="font-weight-bold">{{ msg.user }}: </span>{{ msg.message }}</p>
-              </div>
-          </div>
+        <div class="test" v-for="(msg, index) in test" :key="index">
+          <p>
+            <span class="font-weight-bold">{{ msg.username }}:</span>
+            {{ msg.content }}
+          </p>
+        </div>
+        <div class="messages" v-for="(msg, index) in messages" :key="index">
+          <p>
+            <span class="font-weight-bold">{{ msg.user }}:</span>
+            {{ msg.message }}
+          </p>
+        </div>
       </div>
-      <div class="card-footer">
-          <form @submit.prevent="sendMessage">
-              <div class="gorm-group">
-                  <label for="user">User:</label>
-                  <input type="text" v-model="user" class="form-control">
-              </div>
-              <div class="gorm-group pb-3">
-                  <label for="message">Message:</label>
-                  <input type="text" v-model="message" class="form-control">
-              </div>
-              <button type="submit" class="btn btn-success">Send</button>
-          </form>
-            <!-- <p>{{messages}}</p> -->
-      </div>
+    </div>
+    <div class="card-footer">
+      <form @submit.prevent="sendMessage">
+        <div class="gorm-group">
+          <label for="user">User:</label>
+          <input type="text" v-model="user" class="form-control" />
+        </div>
+        <div class="gorm-group pb-3">
+          <label for="message">Message:</label>
+          <input type="text" v-model="message" class="form-control" />
+        </div>
+        <button type="submit" class="btn btn-success">Send</button>
+      </form>
+      <!-- <p>{{messages}}</p> -->
+    </div>
+    <!-- <div class="btn btn-danger mt-4" @click="logout">ログアウト</div> -->
   </div>
 </template>
 
 <script>
 /* eslint-disable no-console */
 
-import io from 'socket.io-client';
+import io from "socket.io-client";
 import axios from "../common/axios-token.js";
 
 export default {
-    name: 'Chat',
-    data() {
-        return {
-            user: '',
-            message: '',
-            messages: [],
-            socket : io('localhost:3001'),
+  name: "Chat",
+  data() {
+    return {
+      user: "",
+      message: "",
+      messages: [],
+      socket: io("localhost:3001"),
 
-            test: ""
-        }
-    },
-    methods: {
-        sendMessage(e) {
-            e.preventDefault();
-            
-            this.socket.emit('SEND_MESSAGE', {
-                user: this.user,
-                message: this.message
-            });
-            this.message = ''
-        }
-    },
-    mounted() {
-        this.socket.on('MESSAGE', (data) => {
-            this.messages = [...this.messages, data];
-            // you can also do this.messages.push(data)
+      test: ""
+    };
+  },
+  methods: {
+    sendMessage(e) {
+      e.preventDefault();
+      if (this.message!="") {
+        this.socket.emit("SEND_MESSAGE", {
+          user: this.user,
+          message: this.message
         });
-        
-        // ローカルストレージでダブルクォーテーションが前後についてるからカット
-        axios.get("todo/")
-            .then(response => {
-            this.test = response.data;
-        });
+        this.message = "";
+      }
     }
-}
+  },
+  mounted() {
+    this.socket.on("MESSAGE", data => {
+      this.messages = [...this.messages, data];
+      // you can also do this.messages.push(data)
+    });
+
+    axios.get("todo/").then(response => {
+      this.test = response.data;
+    });
+  }
+};
 </script>
 
 <style>
