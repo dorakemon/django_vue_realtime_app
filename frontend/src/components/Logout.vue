c2582e9ac4515a43d8eecba94203665e138778f9
dorakemon <ken0326w@gmail.com>
2020-03-03 04:59:13 +0900

    ログアウトコンポ―ト

---
+++ frontend/src/components/Logout.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn  @click="logout" color="error">Logout</v-btn>
+</template>
+
+<script>
+export default {
+  name: "Logout",
+
+  data () {
+    return {
+
+    }
+  },
+  
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    },
+  }
+}
+</script>
